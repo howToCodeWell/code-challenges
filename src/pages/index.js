29a77e255c4f0235767bcb16437185a907af179f
peterfisher <c3T-y7J-vo3-qpg>
2019-12-05 08:07:55 +0000

    Adding links to previous challenges and adding a layout to each challenge

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,20 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Current coding challenge</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+    <h2>Previous Coding Challeges</h2>
+    <ul>
+        <li><Link to="/2019/december">December 2019</Link></li>
+        <li><Link to="/2019/november">November 2019</Link></li>
+    </ul>
   </Layout>
 )
 
