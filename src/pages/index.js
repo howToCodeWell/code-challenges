a2e880b23974acc668ac7db65c617f955fb7601b
peterfisher <c3T-y7J-vo3-qpg>
2019-12-09 18:04:23 +0000

    Updating index to show current challenge and adding previous challenges list Fixes #12

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
+import PostLink from "../components/post-link"
+import CurrentChallenge from "../components/current-challenge"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Current coding challenge</h1>
-
-    <p>TODO - Add content</p>
-
-    <h2>Previous coding challenges</h2>
-    <ul>
-        <li><Link to="/2019/december">December 2019</Link></li>
-        <li><Link to="/2019/november">November 2019</Link></li>
-    </ul>
-  </Layout>
-)
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+  return (
+    <Layout>
+      <h1>Current challenge</h1>
+    <CurrentChallenge/>
+        <h1>Previous challenges</h1>
+    <div>{Posts}</div>
+    </Layout>
+  )
+}
 
 export default IndexPage
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      skip: 1
+      ) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "DD/MM/YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
