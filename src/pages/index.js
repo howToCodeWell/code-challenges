41b572a4072b094ff4792a1bbfffaaebdf27d079
peter fisher <hello@websomatic.co.uk>
2019-12-10 08:33:43 +0000

    Removed SEO component Added page 2 col flex structure Added submit button Fixes #10 #

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { graphql } from "gatsby"
 import PostLink from "../components/post-link"
 import CurrentChallenge from "../components/current-challenge"
 import Layout from "../components/layout";
-import SEO from "../components/seo";
 
 const IndexPage = ({
   data: {
@@ -15,10 +14,19 @@ const IndexPage = ({
     .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
   return (
     <Layout>
-      <h1>Current challenge</h1>
-    <CurrentChallenge/>
-        <h1>Previous challenges</h1>
-    <div>{Posts}</div>
+      <div class="flex-non sm:flex ">
+        <div class="flex-none sm:flex-1 md:flex-auto  m-2">
+          <h1>Current challenge</h1>
+          <CurrentChallenge/>
+          <div class="text-center md:text-left m-1 pt-5 pb-5" >
+          <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 no-underline" href={"https://howtocodewell.net/discord"} >Submit your challenge</a>
+              </div>
+            </div>
+        <div class="flex-none sm:flex-1 md:flex-auto  m-2">
+          <h1>Previous challenges</h1>
+          <div>{Posts}</div>
+        </div>
+      </div>
     </Layout>
   )
 }
