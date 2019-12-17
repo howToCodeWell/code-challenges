84d09f2a1ecf9e5b4c41aa9bea3e0325e58b65c1
peterfisher <c3T-y7J-vo3-qpg>
2019-12-17 18:04:19 +0000

    Adding alt and rel

--- src/components/challenger-link.js
+++ src/components/challenger-link.js
@@ -1,10 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
 const ChallengeLink = ({ challenger }) => (
   <div className="flex">
-    <a href={challenger} target="_blank">
+    <a href={challenger} target="_blank" rel="noopener noreferrer">
         <div className="flex">
-      <img className="profile-image" src={"https://github.com/" + challenger.replace(/https:\/\/github.com\/(.*)\/(.*)/i, '$1') + ".png"} align={'test'}/>
+      <img  alt={challenger.replace(/https:\/\/github.com\/(.*)\/(.*)/i, '$1')} className="profile-image" src={"https://github.com/" + challenger.replace(/https:\/\/github.com\/(.*)\/(.*)/i, '$1') + ".png"} align={'test'}/>
         <div className="ml-2 mt-3">
         {challenger.replace(/https:\/\/github.com\/(.*)\/(.*)/i, '$1')}
         </div>
