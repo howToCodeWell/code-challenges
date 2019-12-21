f946f9b49548e2d7c5b8004ecc1dd81da2db8249
peterfisher <c3T-y7J-vo3-qpg>
2019-12-21 09:08:57 +0000

    Updating packages and adding HTCW menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {},
+    colors:{
+      'howtocodewell': '#14252C',
+      'text-white': '#FFF'
+    }
   },
   variants: {},
   plugins: []
