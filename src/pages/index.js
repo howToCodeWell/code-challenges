0faa8e5127a1947e5bea85543f695255728f91e3
peterfisher <c3T-y7J-vo3-qpg>
2020-01-05 10:52:47 +0000

    Fixing styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,35 @@
 import React from "react"
-import { graphql } from "gatsby"
+import {graphql} from "gatsby"
 import PostLink from "../components/post-link"
 import CurrentChallenge from "../components/current-challenge"
 import Layout from "../components/layout";
 
 const IndexPage = ({
-  data: {
-    allMarkdownRemark: { edges },
-  },
-}) => {
-  const Posts = edges
-    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
-    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
-  return (
-    <Layout>
-      <div className="flex-non sm:flex ">
-        <div className="flex-none sm:flex-1 md:flex-auto mr-5">
-          <CurrentChallenge/>
-          <div className="text-center md:text-left m-1 pt-5 pb-5" >
-          <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 no-underline" href={"https://howtocodewell.net/discord"} >Submit your challenge</a>
-              </div>
+                       data: {
+                           allMarkdownRemark: {edges},
+                       },
+                   }) => {
+    const Posts = edges
+        .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+        .map(edge => <PostLink key={edge.node.id} post={edge.node}/>);
+    return (
+        <Layout>
+            <div className="flex-non sm:flex ">
+                <div className="flex-none sm:flex-1 md:flex-auto mr-5">
+                    <CurrentChallenge/>
+                    <div className="text-center md:text-left m-1 pt-5 pb-5">
+                        <a className="inline-block border rounded py-1 px-3 hover:no-underline"
+                           href={"https://howtocodewell.net/discord"}>Submit your challenge</a>
+                    </div>
+                </div>
+                <div className="flex-none sm:flex-1 md:flex-auto mt-2 mb-2">
+                    <h1>Previous challenges</h1>
+                    <nav>{Posts}</nav>
+                </div>
             </div>
-        <div className="flex-none sm:flex-1 md:flex-auto mt-2 mb-2">
-          <h1>Previous challenges</h1>
-          <nav>{Posts}</nav>
-        </div>
-      </div>
-    </Layout>
-  )
-}
+        </Layout>
+    )
+};
 
 export default IndexPage
 export const pageQuery = graphql`
@@ -50,4 +51,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
