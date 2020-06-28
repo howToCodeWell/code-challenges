9ddf37e1744b96f01e2f7d656f42f7cef816efbf
peter fisher <hello@websomatic.co.uk>
2020-06-28 16:31:56 +0100

    Fixing icons

--- src/components/current-challenge.js
+++ src/components/current-challenge.js
@@ -1,8 +1,9 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import {StaticQuery, graphql} from "gatsby"
+
 export default () => (
-  <StaticQuery
-    query={graphql`
+    <StaticQuery
+        query={graphql`
   query {
     allMarkdownRemark(
         sort: { order: DESC, fields: [frontmatter___date] }
@@ -24,19 +25,21 @@ export default () => (
     }
   }
 `}
-    render={data => (
-      <header>
-        <h1>{data.allMarkdownRemark.edges[0].node.frontmatter.title}</h1>
-          <div className="flex devicons mb-4">
-              <i className="devicon-javascript-plain colored"></i>
-              <i className="devicon-css3-plain-wordmark colored"></i>
-              <i className="devicon-html5-plain-wordmark colored"></i>
-          </div>
-          <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: data.allMarkdownRemark.edges[0].node.html }}
-        />
-      </header>
-    )}
-  />
+
+        render={data => (
+            <>
+                <header>
+                    <h1>{data.allMarkdownRemark.edges[0].node.frontmatter.title}</h1>
+                    <div className="flex devicons mb-4">
+                        {data.allMarkdownRemark.edges[0].node.frontmatter.technologies.map(tech => <i key={tech}
+                                                                                                      className={`devicon-${tech}-plain colored`}/>)}
+                    </div>
+                    <div
+                        className="blog-post-content"
+                        dangerouslySetInnerHTML={{__html: data.allMarkdownRemark.edges[0].node.html}}
+                    />
+                </header>
+            </>
+        )}
+    />
 )
